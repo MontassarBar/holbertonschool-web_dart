27aea2d5aecceabba4ab5afc83b22b376f2bf91a
MontassarBar <montassarbarbouchi7@gmail.com>
2023-07-18 18:28:50 +0200

    Basketball Shots

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,7 +1,6 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
     int teamAScore = (teamA['Free throws'] ?? 0) + ((teamA['2 pointer'] ?? 0) * 2) + ((teamA['3 pointer'] ?? 0) * 3);
     int teamBScore = (teamB['Free throws'] ?? 0) + ((teamB['2 pointer'] ?? 0) * 2) + ((teamB['3 pointer'] ?? 0) * 3);
-    print(teamAScore);
     if (teamAScore < teamBScore) {
         return 2;
     }
