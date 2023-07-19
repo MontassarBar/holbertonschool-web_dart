b785af047b2670be1c87b35305c1d1b426db3ba2
MontassarBar <montassarbarbouchi7@gmail.com>
2023-07-19 18:00:03 +0200

    Longest Unique Substring

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -1,12 +1,16 @@
 String longestUniqueSubstring(String str) {
     String s = '';
+    String ss = '';
     for (var x = 0; x < str.length; x++){
       if (s.contains(str[x])){
         s = s.substring(s.indexOf(str[x]) + 1) + str[x];
       }
       else {
         s += str[x];
+        if (s.length > ss.length) {
+            ss = s;
+        }
       }
     }
-  return s;
+  return ss;
 }
