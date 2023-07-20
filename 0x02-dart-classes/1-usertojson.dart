d8cfd8b2af6aa4ce32af573fac89039ffe1a7ad1
MontassarBar <montassarbarbouchi7@gmail.com>
2023-07-20 04:40:43 +0200

    User to Json

--- 0x02-dart-classes/1-usertojson.dart
+++ 0x02-dart-classes/1-usertojson.dart
@@ -2,6 +2,7 @@ class User {
   String name = '';
   int age = 0;
   double height = 0;
+  User({required this.name, required this.age, required this.height});
   toJson() {
     return {'name': name, 'age': age, 'height': height};
   }
