638ed714db6e046b15ca04a4d9bad609ebe822f2
MontassarBar <montassarbarbouchi7@gmail.com>
2023-07-21 19:47:15 +0200

    Password

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -8,13 +8,11 @@ class Password {
       if (password[x].toUpperCase() == password[x]){
         u = 1;
       }
-    }
-    for (var x = 0; x < password.length; x++){
       if (password[x].toLowerCase() == password[x]){
         l = 1;
       }
     }
-    if (password.length >= 8 && password.length <= 16 && u == 1 && l == 1) return true;
+    if (password.length >= 8 && password.length <= 16 && u == 1 && l == 1 && password.contains(new RegExp(r'[0-9]'))) return true;
 }
   @override
   String toString() {
