a3e238ca98598805e205c95440f6e09f60074ebb
MontassarBar <montassarbarbouchi7@gmail.com>
2023-07-21 20:37:05 +0200

    Inheritance

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -16,7 +16,7 @@ class User extends Password {
     return {'id': id, 'name': name, 'age': age, 'height': height};
   }
   static User fromJson(Map<dynamic, dynamic> userJson){
-    return User(id: userJson['id'], name: userJson['name'], age: userJson['age'], height: userJson['height'], user_password: userJson["user_password"],);
+    return User(id: userJson['id'], name: userJson['name'], age: userJson['age'], height: userJson['height'], user_password: userJson['user_password']);
   }
   String toString(){
     return 'User(id : $id ,name: $name, age: $age, height: $height, Password: ${isValid()})';
