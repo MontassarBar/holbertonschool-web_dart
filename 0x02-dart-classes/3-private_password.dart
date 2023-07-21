b6f1f6347c18611fb317b1270dfc87f3e0fdb47d
MontassarBar <montassarbarbouchi7@gmail.com>
2023-07-21 19:51:42 +0200

    Private Password

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,23 @@
+class Password {
+  String _password = "";
+   Password({required String password}): _password = password;
+  
+  isValid() {
+    int u = 0;
+    int l = 0;
+    for (var x = 0; x < _password.length; x++){
+      if (_password[x].toUpperCase() == _password[x]){
+        u = 1;
+      }
+      if (_password[x].toLowerCase() == _password[x]){
+        l = 1;
+      }
+    }
+    if (_password.length >= 8 && _password.length <= 16 && u == 1 && l == 1 && _password.contains(RegExp(r'[0-9]'))) return true;
+    return false;
+}
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+}
