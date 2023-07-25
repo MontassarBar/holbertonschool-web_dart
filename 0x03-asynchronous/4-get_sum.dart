e178012823e5404c77f9c120a75f710d82c05dac
MontassarBar <montassarbarbouchi7@gmail.com>
2023-07-25 17:21:56 +0200

    Calcul Total

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,20 @@
+import '4-util.dart';
+import 'dart:convert';
+
+
+calculateTotal() async {
+  try {
+    var userData = await fetchUserData();
+    Map data = jsonDecode(userData);
+    var orders = await fetchUserOrders(data['id']);
+    double total = 0.0;
+    for (var order in jsonDecode(orders)) {
+      var orderPrice = await fetchProductPrice(order);
+      total += double.parse(orderPrice);
+    }
+    return (total);
+  }
+  catch (err){
+    return(-1);
+  }
+}
