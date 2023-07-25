c70e247bfa58d3ea810bfe318e4fb36e35f57b4d
MontassarBar <montassarbarbouchi7@gmail.com>
2023-07-25 15:57:27 +0200

    Get User

---
+++ 0x03-asynchronous/2-get_user.dart
@@ -0,0 +1,11 @@
+import '2-util.dart';
+
+Future<void> getUser() async {
+  try {
+    var user = await fetchUser();
+    print(user);
+  }
+  catch (err) {
+    print('error caught: $err');
+  }
+}
